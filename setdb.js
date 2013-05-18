39744a14135dd14b1f7917b69bd83fd08445b874
Pasquale Boemio <boemianrapsodi@gmail.com>
2013-05-18 19:56:54 +0200

    unique key

--- setdb.js
+++ setdb.js
@@ -5,12 +5,21 @@ var octodb = require('./lib/octodb.js');
 octodb.connectToDb(function(err, db) {
   if (err) throw err;
   db.collection('users', { strict: true }, function(err, collection) {
-    if (!err) return;
+    if (!err) {
+      console.log('users exists!');
+      db.close();
+      return;
+    }
     db.collection('users', function(err, collection) {
       console.log('Initializing users collection...');
-      collection.insert({ access_token: 'TOKEN' }, function(err, result) {
-        console.log('Inserted ' + JSON.stringify(result));
-        db.close();
+      console.log('Setting token uniqueness');
+      var keys = { access_token: 1 };
+      var params = { unique: true };
+      collection.ensureIndex(keys, params, function(err, replies) {
+        collection.insert({ access_token: 'TOKEN' }, function(err, result) {
+          console.log('Inserted ' + JSON.stringify(result));
+          db.close();
+        });
       });
     });
   });
