308124170190f16037edfcaa94bea7a5925a8d35
Pasquale Boemio <boemianrapsodi@gmail.com>
2013-05-25 16:31:05 +0200

    auth redus

--- web.js
+++ web.js
@@ -9,15 +9,17 @@ var settings = require('./settings.js').loadSettings();
 var RedisStore = require('connect-redis')(express);
 
 var UserController = require('./lib/user_controller.js');
-//var RepoController = require('./lib/repo_controller.js');
 
 app.use(express.static(settings.staticfolder));
 app.use(express.cookieParser());
 var redisUrl = require('url').parse(settings.redisurl);
 app.use(express.session({
   secret: settings.secret,
-  store: new RedisStore({host: redisUrl.hostname, port: redisUrl.port})
-  //store: new express.session.MemoryStore
+  store: new RedisStore({
+    host: redisUrl.hostname,
+    port: redisUrl.port,
+    pass: redisUrl.auth ? redisUrl.auth.split(':')[1] : undefined
+  })
 }));
 app.set('views', settings.viewsfolder);
 app.set('view engine', 'ejs');
